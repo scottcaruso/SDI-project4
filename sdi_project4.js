0cab64a755dfdaa8ca1b8c4e1e3addb0d0278bb1
Scott Caruso <scottcaruso@fullsail.edu>
2012-04-24 00:22:46 -0400

    Added function 1 - formatting a number to use a special number of decimal places.

--- sdi_project4.js
+++ sdi_project4.js
@@ -2,33 +2,48 @@
 //Project: Project 4
 // Class, Term: SDI, 1204
 
-console.log("I am so awesome!");
+var scottLib = function(){
 
 //Part 1
 //Number
 //Format a number to use a specific number of decimal places, as for money: 2.1 → 2.10
 
-var formatNumber = function(number){
-//Something happens in here to take that number and reformat it
-	var rounded = //whatever we did up there
-	return rounded
-	};
+	var formatNumber = function(number){
+		var localNumber = new String(number);
+		var length = localNumber.length;
+		var decimalPlace = localNumber.indexOf(".");
+			if (decimalPlace >= 0) {
+				var wholeNumber = localNumber.substr(0,decimalPlace);
+		var decimal = localNumber.substr(decimalPlace,length);
+		var decimalLength = decimal.length;
+			if (decimalLength > 3){
+				var newDecimal = decimal.substr(0,3);
+			} else if (decimalLength = 3) {
+				var newDecimal = decimal;
+			} else if (decimalLength = 2) {
+				var zero = new String(0);
+				var newDecimal = decimal.concat(zero);
+			} else if (decimalLength <= 1) {
+				var newDecimal = new String(00)
+			};
+		var convertedNumber = wholeNumber.concat(newDecimal);	
+			} else {
+				var newDecimal = new String(".00");
+				var convertedNumber = localNumber.concat(newDecimal);
+			};
+		console.log(convertedNumber);
 };
-
-formatNumber();
 
 //Part 2
 //Number
 //Find the number of hours or days difference between two dates.
+//var daysBetween = function(earlierDate,laterDate){Something happens here to turn this into usable data.};
 
-var timeDifference = function("earlierDate","laterDate"){
-//Something happens here to turn this into usable data.
-//Do some math.
-	var hoursReturn
-	var datesReturn
 	return {
-	//Return an object
-	};
+		"formatNumber" : formatNumber,
+		"daysBetween": []
+		};
 };
 
-timeDifference("Date 1","Date 2");
+var lib = scottLib(); 
+console.log(lib.formatNumber(14));
