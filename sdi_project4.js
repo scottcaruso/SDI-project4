ef445c34c026fd94bcba2c1492f15c64a5ef24e8
Scott Caruso <scottcaruso@fullsail.edu>
2012-04-23 22:07:39 -0400

    Adding 2 dummy/placeholder functions to the library for the two I am going to start on.

--- sdi_project4.js
+++ sdi_project4.js
@@ -1,1 +1,34 @@
-alert("JavaScript works!");
+//Name: Scott Caruso
+//Project: Project 4
+// Class, Term: SDI, 1204
+
+console.log("I am so awesome!");
+
+//Part 1
+//Number
+//Format a number to use a specific number of decimal places, as for money: 2.1 → 2.10
+
+var formatNumber = function(number){
+//Something happens in here to take that number and reformat it
+	var rounded = //whatever we did up there
+	return rounded
+	};
+};
+
+formatNumber();
+
+//Part 2
+//Number
+//Find the number of hours or days difference between two dates.
+
+var timeDifference = function("earlierDate","laterDate"){
+//Something happens here to turn this into usable data.
+//Do some math.
+	var hoursReturn
+	var datesReturn
+	return {
+	//Return an object
+	};
+};
+
+timeDifference("Date 1","Date 2");
