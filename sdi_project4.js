04f8f40708fb5b20a8f0ddc2a37e9225031e9216
Scott Caruso <scottcaruso@fullsail.edu>
2012-04-24 23:41:54 -0400

    adding third function

--- sdi_project4.js
+++ sdi_project4.js
@@ -8,35 +8,35 @@ var scottLib = function(){
 //Number
 //Format a number to use a specific number of decimal places, as for money: 2.1 → 2.10
 
-	var formatNumber = function(number){
-		var localNumber = new String(number);
-		var length = localNumber.length;
-		var decimalPlace = localNumber.indexOf(".");
-			if (decimalPlace >= 0) {
-				var wholeNumber = localNumber.substr(0,decimalPlace);
-		var decimal = localNumber.substr(decimalPlace,length);
-		var decimalLength = decimal.length;
-			if (decimalLength > 3){
-				var newDecimal = decimal.substr(0,3);
-			} else if (decimalLength = 3) {
-				var newDecimal = decimal;
-			} else if (decimalLength = 2) {
-				var zero = new String(0);
-				var newDecimal = decimal.concat(zero);
-			} else if (decimalLength <= 1) {
-				var newDecimal = new String(00)
-			};
-		var convertedNumber = wholeNumber.concat(newDecimal);	
-			} else {
-				var newDecimal = new String(".00");
-				var convertedNumber = localNumber.concat(newDecimal);
-			};
+var formatNumber = function(number){
+	var localNumber = new String(number);
+	var length = localNumber.length;
+	var decimalPlace = localNumber.indexOf(".");
+		if (decimalPlace >= 0) {
+			var wholeNumber = localNumber.substr(0,decimalPlace);
+	var decimal = localNumber.substr(decimalPlace,length);
+	var decimalLength = decimal.length;
+		if (decimalLength > 3){
+			var newDecimal = decimal.substr(0,3);
+		} else if (decimalLength = 3) {
+			var newDecimal = decimal;
+		} else if (decimalLength = 2) {
+			var zero = new String(0);
+			var newDecimal = decimal.concat(zero);
+		} else if (decimalLength <= 1) {
+			var newDecimal = new String(00)
+		};
+	var convertedNumber = wholeNumber.concat(newDecimal);	
+		} else {
+			var newDecimal = new String(".00");
+			var convertedNumber = localNumber.concat(newDecimal);
+		};
 	return convertedNumber
 };
 
 //Part 2
-//Number
-//Find the number of hours or days difference between two dates.
+//String
+//Determine if a string is a phone number
 var isStringPhoneNumber = function(number){
 	var string = new String (number);
 	var firstHyphen = string.indexOf("-");
@@ -103,12 +103,23 @@ var isStringPhoneNumber = function(number){
 	return isValid
 };
 
+//Part 3
+//Number
+//Given a string version of a number such as "42", return the value as an actual Number, such as 42.
+
+var convertToNumber = function(string){
+	var realNumber = parseFloat(string);
+	return realNumber
+};
+
 	return {
 		"formatNumber" : formatNumber,
-		"isStringPhoneNumber": isStringPhoneNumber
+		"isStringPhoneNumber": isStringPhoneNumber,
+		"convertToNumber" : convertToNumber
 		};
 };
 
 var lib = scottLib(); 
 console.log(lib.formatNumber(14));
 console.log(lib.isStringPhoneNumber("203-867-5309"));
+console.log(lib.convertToNumber("3.75 pounds"));
